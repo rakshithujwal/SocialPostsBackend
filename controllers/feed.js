c3c1171903fb8b34b564ae0fdceccb0b7969ed62
rakshithujwal11 <rakshithujwal11@gmail.com>
2025-03-20 13:26:49 +0530

    added Post Model

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,45 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "/images/tiger.jpeg",
+        creator: {
+          name: "Rakshith",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation Failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
+
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: "Rakshith",
+      },
+      createdAt: new Date(),
+    },
   });
 };
