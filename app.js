96516f911e6b38166788a7e3018327789ff67cf6
rakshithujwal11 <rakshithujwal11@gmail.com>
2025-03-20 19:12:12 +0530

    added authentication

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const multer = require("multer");
 const DB_URL = process.env.DB_URL;
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -49,13 +50,15 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log("Error Handling Middleware===>", error);
 
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
